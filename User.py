c792ca549a66fea78f439014a379acfcf240ed21
MohamadHassanSalameh421 <mohamadhassansalameh@gmail.com>
2024-07-20 08:40:53 +0300

    Changed something

--- User.py
+++ User.py
@@ -3,7 +3,17 @@ class User():
     def __init__(self, name, location) -> None:
         self.name = name
         self.location = location
-        
+        self.interests = []
+
+    
+    def add_interests(self):
+        decision = 0
+        while decision == 0:
+            interest = input("Write your interests: ")
+            self.interests.append(interest)
+            print("0: To continue, 1: To Stop")
+            decision = int(input(""))
+
 
 
 
