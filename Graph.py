0d0dbc6e35601fb4748a495f8504d412aa21d94b
MohamadHassanSalameh421 <mohamadhassansalameh@gmail.com>
2024-07-20 07:15:55 +0300

    Implemented the add_friend method

--- Graph.py
+++ Graph.py
@@ -1,4 +1,4 @@
-class Graph():
+class DiGraph(): #A directed graph
 
     def __init__(self):
 
@@ -6,14 +6,31 @@ class Graph():
         self.vertices = {}
     
 
-    def add_user(self, user):
+    def add_user(self, user): #In here we add users to the social network
         if user not in self.vertices:
 
             self.vertices[user] = len(self.graph)
 
             for row in self.graph:
-                self.graph.append(0)
+                row.append(0)
 
             self.graph.append([0] * (len(self.graph) + 1))
+    
 
-            
+
+    def add_friends(self, user1, user2, distance=0): #We add friends with distances to user2 (user1 -> user2)
+        if user1 and user2 in self.vertices:
+
+            self.graph[self.vertices[user2]][self.vertices[user1]] = distance
+
+
+
+
+
+
+    
+
+
+
+
+
