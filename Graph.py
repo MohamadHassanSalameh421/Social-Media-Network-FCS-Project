52ced7a1b0476f109e9f1568a816824e628b0f1a
MohamadHassanSalameh421 <mohamadhassansalameh@gmail.com>
2024-07-20 07:28:18 +0300

    Implemented the remove_user method

--- Graph.py
+++ Graph.py
@@ -32,6 +32,21 @@ class DiGraph(): #A directed graph, In this weighted Graph 0 means no connection
     
     
 
+    def remove_user(self, user1):
+
+        if user1 in self.vertices:
+
+            del self.graph[self.vertices[user1]]
+
+            for row in self.graph:
+                row.pop()
+            
+            for k, v in self.vertices.items():
+                if v > self.vertices[user1]:
+                    self.vertices[k] = v - 1
+                    
+            del self.vertices[user1]
+
 
 
 
