f0d8440cc1d63333c6e61053d8a78a36a3f232d5
MohamadHassanSalameh421 <mohamadhassansalameh@gmail.com>
2024-07-20 08:09:27 +0300

    Implemented The DFS algorithmm

--- Graph.py
+++ Graph.py
@@ -46,11 +46,44 @@ class DiGraph(): #A directed graph, In this weighted Graph 0 means no connection
                     self.vertices[k] = v - 1
                     
             del self.vertices[user1]
+    
+
+
+    def DFS(self, start):
+
+        visited = set()
+        i = self.vertices[start]
+
+        def recursive_call(i, visited):
+            for k, v in self.vertices.items():
+                if v == i:
+                    print(k)
+                    print('|')
+
+            visited.add(i)
+
+            for r in range(len(self.graph)):
+                if self.graph[i][r] > 1 and r not in visited:
+                    recursive_call(r, visited)
+
+        recursive_call(i, visited)
+        print("Null")
+
+        
 
 
 
 
 
+G = DiGraph()
+G.add_user("Alice")
+G.add_user("Bob")
+G.add_user("Ali")
+G.add_friends("Alice", "Bob", 8)
+G.add_friends("Bob", "Alice", 8)
+G.add_friends("Bob", "Ali", 2)
+
+G.DFS("Ali")
 
     
 
