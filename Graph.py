837c5e574ef7935569a398c5f98ba90e13bfe679
MohamadHassanSalameh421 <mohamadhassansalameh@gmail.com>
2024-07-20 06:56:10 +0300

    Implemented the add_user function

--- Graph.py
+++ Graph.py
@@ -5,3 +5,15 @@ class Graph():
         self.graph = []
         self.vertices = {}
     
+
+    def add_user(self, user):
+        if user not in self.vertices:
+
+            self.vertices[user] = len(self.graph)
+
+            for row in self.graph:
+                self.graph.append(0)
+
+            self.graph.append([0] * (len(self.graph) + 1))
+
+            
